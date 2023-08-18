1a612b305a6e70a7babdf9c51490f53b8593807e
wtfia31 <fiatriokta31@gmail.com>
2023-08-18 18:05:18 +0700

    first commit

---
+++ ConstraintExercise-Fia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConstraintExercise-Fia
+//
+//  Created by DDL13 on 18/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
